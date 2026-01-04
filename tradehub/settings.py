d181e7bff2f83abbd079d54a69e25bfc61ff23cf
Preet Parmar <preet@Preets-MacBook-Air.local>
2026-01-04 13:15:05 +0530

    Fix Cloudinary + WhiteNoise conflict causing 500

--- tradehub/settings.py
+++ tradehub/settings.py
@@ -7,18 +7,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
+    "localhost,127.0.0.1,commercegrid.up.railway.app"
 ).split(",")
+
 CSRF_TRUSTED_ORIGINS = [
     "https://commercegrid.up.railway.app",
 ]
 
 INSTALLED_APPS = [
     "cloudinary",
-    "cloudinary_storage",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
